fce1d3d371436bc11098b679039f931bbae906a2
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-06-17 12:55:15 +0430

    add template

---
+++ vue.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		karname: [
+			{dars: 'ریاضی', nomredigit: '۱۴', nomrealpha: 'چهارده'},
+			{dars: 'فیزیک', nomredigit: '۱۶', nomrealpha: 'شانزده'},
+			{dars: 'زیست', nomredigit: '۱۸', nomrealpha: 'هجده'},
+		]
+	}
+})
