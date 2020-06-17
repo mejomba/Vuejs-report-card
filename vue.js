e347a159a132a3e02e224048d4bf4dab2c44d8e2
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-06-17 14:47:41 +0430

    avg computed

--- vue.js
+++ vue.js
@@ -2,9 +2,28 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		karname: [
-			{dars: 'ریاضی', nomredigit: '۱۴', nomrealpha: 'چهارده'},
-			{dars: 'فیزیک', nomredigit: '۱۶', nomrealpha: 'شانزده'},
-			{dars: 'زیست', nomredigit: '۱۸', nomrealpha: 'هجده'},
-		]
+			{dars: 'ریاضی', nomredigit: 15, nomrealpha: 'چهارده'},
+			{dars: 'فیزیک', nomredigit: 16, nomrealpha: 'شانزده'},
+			{dars: 'زیست', nomredigit: 18, nomrealpha: 'هجده'},
+		],
+		darsName : '',
+		darsDigit : '',
+		darsAlpha : '',
+	},
+
+	methods: {
+		insertData() {
+			this.karname.push({dars: this.darsName, nomredigit: parseInt(this.darsDigit), nomrealpha: this.darsAlpha})
+		}
+	},
+
+	computed: {
+		pga() {
+			sum = 0
+			for (var i = this.karname.length - 1; i >= 0; i--) {
+				sum += this.karname[i].nomredigit
+			}
+			return (sum / this.karname.length).toFixed(2)
+		}
 	}
 })
